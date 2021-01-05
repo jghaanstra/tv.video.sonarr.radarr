8d8e2c2b1f23a9053ae457a4ab0e27f0ef67e8cc
jghaanstra <jghaanstra@gmail.com>
2021-01-05 19:14:02 +0100

    Release 3.0.1

--- api.js
+++ api.js
@@ -4,8 +4,12 @@ const Util = require('/lib/util.js');
 
 module.exports = {
   async eventTrigger({homey, body, params}) {
-    const util = new Util({homey: homey});
-    const result = await this.util.processNotification(params.event, body);
-    return result;
+    try {
+      const util = new Util({homey: homey});
+      const result = await util.processNotification(params.event, body);
+      return result;
+    } catch(error) {
+      console.log(error)
+    }
   }
 }
